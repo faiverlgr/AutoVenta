66494253006c285605a6363ec4de9ded0bd20dcb
faiverlgr <faiverlgr@hotmail.com>
2018-02-07 20:50:01 -0500

    Compilar js con Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,14 @@ let mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
+ mix.js('resources/assets/js/app.js', 'public/js')
+ .sass('resources/assets/sass/app.scss', 'public/css');
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+ mix.scripts([
+     'resources/assets/js/jquery-3.3.1.js',
+     'resources/assets/js/bootstrap.js',
+     'resources/assets/js/adminlte.js'
+    ], 'public/js/app.js')
+ .sass('resources/assets/sass/app.scss', 'public/css');
+
